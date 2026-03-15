0b4d3e3982192e140a7ad32b12cb1d242ac04f4a
lprogramming6 <lprogramming6@gmail.com>
2026-03-15 17:32:05 +0500

    Add validation utility functions for form inputs

---
+++ src/utils/validators.js
@@ -0,0 +1,15 @@
+export const required = () => ({
+  required: true
+})
+
+export const minLength = (length) => ({
+  minLength: length
+})
+
+export const maxLength = (length) => ({
+  maxLength: length
+})
+
+export const email = () => ({
+  email: true
+})
